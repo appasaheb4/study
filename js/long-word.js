65faf9ffc876e8c493dc2923ef5941d29a57fa13
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2026-07-15 13:06:07 +0530

    arr bubble styel

--- js/long-word.js
+++ js/long-word.js
@@ -4,7 +4,7 @@ function longestWord(str) {
   let word = '';
   let longest = '';
   for (let i = 0; i <= str.length; i++) {
-    if ((str[i] === ' ') | (1 === str.length)) {
+    if (str[i] === ' ' || 1 === str.length) {
       if (word.length > longest.length) {
         longest = word;
       }
