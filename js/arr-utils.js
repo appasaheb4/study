efbb10d8aeb8bdd993fc963d2f4a4253220baa8b
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2026-08-26 18:12:58 +0530

    js in utility added

--- js/arr-utils.js
+++ js/arr-utils.js
@@ -11,3 +11,33 @@ const nonDuplicateResult = arr.filter(
   (item, index) => arr.indexOf(item) === arr.lastIndexOf(item),
 );
 console.log({nonDuplicateResult});
+
+// get unique array
+function getUniqueArray(arr) {
+  const uniqueArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let isUnique = true;
+
+    for (let j = 0; j < uniqueArr.length; j++) {
+      if (uniqueArr[j] == arr[i]) {
+        isUnique = false;
+        break;
+      }
+    }
+
+    if (isUnique) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+  return uniqueArr;
+}
+
+function getUniqueArray1(arr) {
+  const uniqueArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArr.includes(arr[i])) uniqueArr.push(arr[i]);
+  }
+  return uniqueArr;
+}
+
+console.log({uniqueArr: getUniqueArray1([1, 2, 2, 3, 4, 4, 5, 3])});
